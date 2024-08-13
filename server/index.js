d0ad953b043250880357dae3af946444517e0d88
gdimascio <guido.dimascio@gmail.com>
2024-08-13 13:35:17 -0300

    first

---
+++ server/index.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get ("/", (req, res) => {
+    res.send("ATA")
+})
+
+app.listen(PORT, () => {
+    console.log("Server listening on http://localhost:" + PORT);
+})
