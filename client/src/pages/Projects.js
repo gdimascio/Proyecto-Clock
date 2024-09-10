d8b3b94d632d20866ca727b8ba0eda9b22a3215e
gdimascio <guido.dimascio@gmail.com>
2024-09-10 16:07:00 -0300

    perf(client): ADD .map to iterate though projects and show content (TBA)

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -4,12 +4,13 @@ import { useSelector } from "react-redux"
 export default function Projects (){
     const projects = useSelector((state) => state.auth.projects)
 
+    const projDisp = projects.map(function(proj) {
+        return <p>{proj.nombre_proy}</p>
+    })
+
     return(
         <div className="logged">
-
-            <p>{`${projects[0].id}`}</p>
-            <p>{`${projects[0].notas}`}</p>
-            <p>{`${projects[0].nombre_proy}`}</p>
+            {projDisp}
         </div>
     )
 }
